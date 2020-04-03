98cee91a74799e09055919fb5d408975482d7ed9
MapeSVK <michal.moravik@icloud.com>
2020-04-03 08:48:13 +0200

    Revert "init"
    This reverts commit 641d3f6c086e4ced6ac1844f4c4d6e935dfe0e1b.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 # Init server
 app = Flask(__name__)
 
+############ DATABASE INIT ############
 # Setup sql-alchemy database URI to locate a database file
 # Database file will be in a root directory
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +16,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///' + \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init database
 db = SQLAlchemy(app)
+# Init marshmallow
+ma = Marshmallow(app)
+
 
 # Run server
 if __name__ == 'main':
