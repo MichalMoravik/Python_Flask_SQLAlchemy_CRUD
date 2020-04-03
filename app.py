641d3f6c086e4ced6ac1844f4c4d6e935dfe0e1b
MapeSVK <michal.moravik@icloud.com>
2020-04-03 08:44:58 +0200

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init server
+app = Flask(__name__)
+
+# Setup sql-alchemy database URI to locate a database file
+# Database file will be in a root directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///' + \
+    os.path.join(basedir, 'db.sqllite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init database
+db = SQLAlchemy(app)
+
+# Run server
+if __name__ == 'main':
+    app.run(debug=True)
